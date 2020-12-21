db7320632239b83a5be0280d557a848877320806
Andrey Zaytsev <and.zaytsev9@gmail.com>
2020-12-21 15:12:24 +0300

    New structure, little body

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 import xml.etree.ElementTree as ET
 
-all_txt = []
-tree = ET.parse('newsafr.xml')
-root = tree.getroot()
-news_all = root.findall('channel/item')
+def read_file(file):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    news_all = root.findall('channel/item')
+    return news_all
 
-for news in news_all:
-    description = news.find('description')
-    description = description.text.split(' ')
-    all_txt += description
-# print(all_txt)
-long_words = []
-for word in all_txt:
-    # print(word)
-    if len(word) > 6:
-        long_words.append(word)
+def get_txt(file):
+    all_txt = []
+    news_all = read_file(file)
+    for news in news_all:
+        description = news.find('description')
+        description = description.text.split(' ')
+        all_txt += description
+    return all_txt
 
+def get_long_words(file):
+    words_list = []
+    txt = get_txt(file)
+    for word in txt:
+        if len(word) > 6:
+            words_list.append(word)
+    return words_list
 
-set_long_words = set(long_words)
+def get_repeats_of_words(file):
+    unique_words = {}
+    long_words = get_long_words(file)
+    for word in long_words:
+        if word not in unique_words:
+            unique_words[word] = 1
+        else:
+            unique_words[word] += 1
+    return unique_words
 
-list = []
-top = []
+def get_sorted_top(file):
+    repeats = get_repeats_of_words(file)
+    tuples_list = list(repeats.items())
+    tuples_list.sort(key=lambda file: file[1], reverse=True)
+    top = []
+    for word in tuples_list[:10]:
+        top.append(word[0])
+    return top
 
-for word in set_long_words:
-    number_of_repeats = long_words.count(word)
-    list.append([word, number_of_repeats])
-
-list.sort(key = lambda file: file[1], reverse=True)
-for word in list[:10]:
-    top.append(word[0])
-
+top = get_sorted_top(file)
 print(top)
